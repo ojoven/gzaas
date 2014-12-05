f3bf8d47946a8c81fbb4e48ce8e56c0c0b94b32f
Mikel Torres <michel@majesticmedia.ca>
2014-12-05 16:24:08 +0100

    some little fixes

--- application/modules/gzaas/models/Gzaas.php
+++ application/modules/gzaas/models/Gzaas.php
@@ -517,7 +517,9 @@ class Gzaas_Model_Gzaas {
 		$params = $this->_getParamsScreenshot($urlKey);
 		$paramsAsArgs = implode(" ", $params);
 		// we must create a script that not only creates the screenshot but that it uploads it to Amazon S3, too.
-		exec("sh " . $pathToScript . " " . $paramsAsArgs . " > /dev/null 2>&1 &");
+		$script = "sh " . $pathToScript . " " . $paramsAsArgs . " > /dev/null 2>&1 &";
+		// TODO Maybe we should retrieve results from the script or something?
+		exec($script);
 	}
 	
 	function _getParamsScreenshot($urlKey) {
