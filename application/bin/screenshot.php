f5d671506faf13b6dddb536c9c4528717ee8337c
Gonzalo Ayuso <gonzalo123@gmail.com>
2014-12-23 16:31:15 +0100

    clean repo

--- application/bin/screenshot.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('loadzend.php');
-
-$urlKey = $argv[1];
-$screenshotModel = new Gzaas_Model_Screenshot();
-$screenshotModel->uploadScreenshotToAmazon($urlKey);
--- library/My/Session/Data.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class My_Session_Data extends Zend_Db_Table_Abstract  
-{
-    protected $_name = 'session';
-}
